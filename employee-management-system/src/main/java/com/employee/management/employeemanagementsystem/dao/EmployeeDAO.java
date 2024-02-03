d951d04c69ea59bed962f5e92681d5dd65b54c5c
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-03 11:07:22 +0100

    Create EmployeeDAO interface

--- employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/dao/EmployeeDAO.java
+++ employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/dao/EmployeeDAO.java
@@ -1,4 +1,13 @@
 package com.employee.management.employeemanagementsystem.dao;
 
+import com.employee.management.employeemanagementsystem.model.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface EmployeeDAO {
+    List<Employee> getEmployees();
+    Optional<Employee> getEmployeeById(int id);
+    boolean addEmployee(Employee employee);
+    boolean deleteEmployee(int id);
 }
