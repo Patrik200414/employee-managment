297d17ef54252649a3842d2027b2802c1a369dbb
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-03 12:45:49 +0100

    Implement getEmployees in EmployeeService

--- employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/service/EmployeeService.java
+++ employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/service/EmployeeService.java
@@ -1,4 +1,18 @@
 package com.employee.management.employeemanagementsystem.service;
 
+import com.employee.management.employeemanagementsystem.dao.EmployeeDAO;
+import com.employee.management.employeemanagementsystem.model.Employee;
+
+import java.util.List;
+
 public class EmployeeService {
+    private final EmployeeDAO employeeDAO;
+
+    public EmployeeService(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    public List<Employee> getEmployees(){
+        return employeeDAO.getEmployees();
+    }
 }
