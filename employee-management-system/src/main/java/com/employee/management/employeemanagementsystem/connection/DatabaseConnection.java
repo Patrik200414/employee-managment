01ef443a8aec93f7d59da338867572d66d1a8a2a
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-03 10:48:07 +0100

    Create interface for database connection

--- employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/connection/DatabaseConnection.java
+++ employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/connection/DatabaseConnection.java
@@ -1,27 +1,8 @@
 package com.employee.management.employeemanagementsystem.connection;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Optional;
 
-@Repository
-public class DatabaseConnection {
-    private final String connectionUrl;
-
-    public DatabaseConnection() {
-        this.connectionUrl = System.getenv("DATABASE_URL");
-    }
-
-    public Optional<Connection> connect(){
-        try {
-            Connection connection = DriverManager.getConnection(connectionUrl);
-            return Optional.of(connection);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
+public interface DatabaseConnection {
+    Optional<Connection> connect();
 }
