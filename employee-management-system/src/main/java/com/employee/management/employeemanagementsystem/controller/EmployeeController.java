6e0308e0d83358cd305c96095553222c3ea6b911
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-04 13:55:01 +0100

    Create Exception handler

--- employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/controller/EmployeeController.java
+++ employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/controller/EmployeeController.java
@@ -1,24 +1,37 @@
 package com.employee.management.employeemanagementsystem.controller;
 
+import com.employee.management.employeemanagementsystem.custom_exception.ExceptionHandler;
 import com.employee.management.employeemanagementsystem.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
     private final EmployeeService employeeService;
+    private final ExceptionHandler exceptionHandler;
 
     @Autowired
-    public EmployeeController(EmployeeService employeeService) {
+    public EmployeeController(EmployeeService employeeService, ExceptionHandler exceptionHandler) {
         this.employeeService = employeeService;
+        this.exceptionHandler = exceptionHandler;
     }
 
 
     @GetMapping("/api/employees")
     public ResponseEntity<?> getEmployees(){
         return ResponseEntity.ok(employeeService.getEmployees());
+    }
+
+    @GetMapping("/api/employees/{id}")
+    public ResponseEntity<?> getEmployeeById(@PathVariable int id){
+        try{
+            return ResponseEntity.ok("");
+        } catch (Exception e){
+            return exceptionHandler.exceptionHandler(e);
+        }
     }
 
 }
---
+++ employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/custom_exception/ExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.employee.management.employeemanagementsystem.custom_exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler({NonExistingEmployeeIdException.class})
+    public ResponseEntity<?> exceptionHandler(Exception exception){
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+}
