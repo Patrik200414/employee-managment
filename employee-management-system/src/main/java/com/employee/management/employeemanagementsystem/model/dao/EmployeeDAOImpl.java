771be9d2c9ab78e2d5bc80cd6d53b627d477aaa7
Patrik200414 <112629896+Patrik200414@users.noreply.github.com>
2024-02-04 14:07:21 +0100

    Implement getEmployeeById in EmployeeDAOImpl

--- employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/model/dao/EmployeeDAOImpl.java
+++ employee-management-system/src/main/java/com/employee/management/employeemanagementsystem/model/dao/EmployeeDAOImpl.java
@@ -9,10 +9,7 @@ import com.employee.management.employeemanagementsystem.model.Level;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -60,6 +57,27 @@ public class EmployeeDAOImpl implements EmployeeDAO{
 
     @Override
     public Optional<Employee> getEmployeeById(int id) {
+        String sql = "SELECT * FROM employees WHERE id = ?;";
+        try{
+            Connection connection = databaseConnection.connect().orElseThrow();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Optional<Employee> optionalEmployee = Optional.empty();
+            while (resultSet.next()){
+                int employeeId = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                Level level = Level.valueOf(resultSet.getString("level"));
+                LocalDate hired = resultSet.getTimestamp("hired").toLocalDateTime().toLocalDate();
+
+                Employee employee = new Employee(employeeId, name, level, hired);
+                optionalEmployee = Optional.of(employee);
+            }
+            connection.close();
+            return optionalEmployee;
+        } catch (Exception e){
+            logger.logError(e.getMessage());
+        }
         return Optional.empty();
     }
 
